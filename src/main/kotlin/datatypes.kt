040ab577a87ae1deaf741c2f6e3d2927a1947c7d
Swabir <swabir49@gmail.com>
2023-03-15 11:40:46 +0300

    data types

---
+++ src/main/kotlin/datatypes.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    //Integer
+    var age:Int = 20
+    //strings(Double quotations only)
+    var name:String ="Ezra"
+    //Double
+    var temperature:Double = 36.51
+    //boolean(True or False)
+    var status = true
+    // float(Uppercase F necessary at the end)
+    var points:Float = 8.5F
+    //Convert Double to Float(Add uppercase F at the end)
+    var marks = 46.587F
+
+}
