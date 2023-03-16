0652d254c57b853d4e29c32e76d83dd73c866890
Swabir <swabir49@gmail.com>
2023-03-16 12:13:22 +0300

    BMI

--- src/main/kotlin/BMI calculator.kt
+++ src/main/kotlin/BMI calculator.kt
@@ -1,7 +1,23 @@
 fun main(args: Array<String>) {
-    var mass = 65
-    var height = 1.7
-    var BMI = mass/height
-    print(BMI)
-
+    println("Enter Your Mass(kg)")
+    var mass:Int = readLine()!!.toInt()
+    println("Enter your height(m)")
+    var height = readLine()!!.toDouble()
+    var BMI = mass/(height * height)
+    if (BMI <=18 ){
+        println(BMI)
+        println("Underweight")
+    }
+    else if(BMI <=29 ){
+        println(BMI)
+        println("Normal Weight")
+    }
+    else if(BMI <=34 ){
+        println(BMI)
+        println("Overweight")
+    }
+    else {
+        println(BMI)
+        println("Obese")
+    }
 }
