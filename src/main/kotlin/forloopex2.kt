37b54854dbea8795785c2744d981276b82bc95c5
Swabir <swabir49@gmail.com>
2023-03-21 11:53:41 +0300

    when statements

--- src/main/kotlin/forloopex2.kt
+++ src/main/kotlin/forloopex2.kt
@@ -1,16 +1,16 @@
 fun main(args: Array<String>) {
     for (range in 1..100)
+        if (range % 15 == 0){
+            println("$range - Fizz")
+        }
 
-
-        if (range % 3 == 0){
+        else if (range % 3 == 0){
             println("$range - Fizzbuzz")
         }
         else if (range % 5 == 0){
             println("$range - buzz")
         }
-        else if (range % 15 == 0){
-            println("$range - Fizz")
-        }
+
         else{
             println("$range Not Divisible")
         }
---
+++ src/main/kotlin/when statements.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    println("Enter number")
+    var number:Int = readLine()!!.toInt()
+
+    when (number){
+        1 -> println("Go to First Floor")
+        2 -> println("Go to Second Floor")
+        3 -> println("Go to Third Floor")
+        4 -> println("Go to Fourth Floor")
+
+        else -> println("NOT AVAILABLE")
+    }
+
+
+}
